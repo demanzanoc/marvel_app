9a84cb1ba7c505287624ac721eff6a10f4ec082a
David Manzano <demanzanoc07@gmail.com>
2024-03-24 13:12:14 -0500

    - The app theme is configured - Starting screen is created

--- app/src/main/java/com/compose/marvelapp/presentation/screens/StartingScreen.kt
+++ app/src/main/java/com/compose/marvelapp/presentation/screens/StartingScreen.kt
@@ -1,27 +1,56 @@
 package com.compose.marvelapp.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.compose.marvelapp.R
 import com.compose.marvelapp.presentation.navigation.AppRoutes
+import com.compose.marvelapp.presentation.theme.MarvelAppTheme
 
 @Composable
 fun StartingScreen(navController: NavHostController) {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.background)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .clickable { navController.navigate(route = AppRoutes.HOME) }
-        )
+    Surface(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+        Box(Modifier.padding(20.dp).fillMaxSize()) {
+            Image(
+                painterResource(id = R.drawable.marvel_icon_image),
+                contentDescription = "marvel image",
+                modifier = Modifier.align(Alignment.Center)
+            )
+            Button(
+                onClick = { navController.navigate(AppRoutes.HOME) },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    MaterialTheme.colorScheme.secondary
+                )
+            ) {
+                Text(text = "Ingresar", Modifier.padding(8.dp), fontSize = 20.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartingScreenPreview() {
+    MarvelAppTheme(darkTheme = false) {
+        StartingScreen(navController = rememberNavController())
     }
 }
--- app/src/main/java/com/compose/marvelapp/presentation/theme/Color.kt
+++ app/src/main/java/com/compose/marvelapp/presentation/theme/Color.kt
@@ -2,10 +2,5 @@ package com.compose.marvelapp.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Red400 = Color(0xFFFF4639)
-val Black500 = Color(0xFF7B7B7B)
-val Red200 = Color(0xFFFC9690)
-
-val Red700 = Color(0xFFE30F0F)
-val Black = Color.Black
-val Red300 = Color(0xFFF56B63)
+val MarvelRed = Color(0xFFED1D24)
+val MarvelYellow = Color(0xFFF6CB12)
